12ba0ab71a07a62058db877b76f74b212b622972
Benjamin Cable <cable.benjamin@gmail.com>
2016-12-02 16:24:29 +0000

    Fixed typos
    Signed-off-by: Benjamin Cable <cable.benjamin@gmail.com>

--- config/config.go
+++ config/config.go
@@ -5,10 +5,10 @@ import "regexp"
 // DirPath is the CLI flag storing the location of the folder you want to run mdg against
 var DirPath string
 
-// SkipMenu is the CLI flag indicating wether or not mdg should skip generating a menu
+// SkipMenu is the CLI flag indicating whether or not mdg should skip generating a menu
 var SkipMenu bool
 
-// CSS is used to store the preloaded github-markdown.html asset
+// CSS is used to store the pre-loaded github-markdown.html asset
 var CSS []byte
 
 // LinksRegExp matches link tokens in the following format
